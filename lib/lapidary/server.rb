f93d9fc9d5fa4c719dc61f85305e0aca84e23048
Patrick W. <patrick@sickday.sh>
2025-04-27 17:08:20 -0400

    chore: indent server

--- lib/lapidary/server.rb
+++ lib/lapidary/server.rb
@@ -3,14 +3,14 @@ module Lapidary
     attr :config
     attr_accessor :updatemode
     attr_accessor :max_players
-    
+
     def initialize(version)
       @version = version
       @updatemode = false
       @max_players = 1000
       setup_logger
     end
-    
+
     def setup_logger
       Logging.color_scheme( 'bright',
         :levels => {
@@ -23,7 +23,7 @@ module Lapidary
         :logger => :white,
         :message => :white
       )
-    
+
       Logging.logger.root.add_appenders(
         Logging.appenders.stdout(
           'stdout',
@@ -33,10 +33,10 @@ module Lapidary
         )),
         Logging.appenders.file('data/logs/development.log', :layout => Logging.layouts.pattern(:pattern => '[%d] %-5l %c: %m\n'))
       )
-      
+
       @log = Logging.logger['server']
     end
-  
+
     def start_config(config)
       @config = config
       init_cache
@@ -46,23 +46,23 @@ module Lapidary
       load_config
       bind
     end
-    
+
     def reload
       HOOKS.clear
       load_hooks
       load_int_hooks
       Lapidary::Net.load_packets
     end
-    
+
     # Load hooks
     def load_hooks
       Dir['./plugins/*.rb'].each {|file| load file }
     end
-    
+
     def load_int_hooks
       Dir['./plugins/internal/*.rb'].each {|file| load file }
     end
-    
+
     def init_cache
       begin
         $cache = Lapidary::Misc::Cache.new('./data/cache/', @version)
@@ -71,37 +71,37 @@ module Lapidary
         Logging.logger['cache'].warn e.to_s
       end
     end
-    
+
     def load_defs
       Lapidary::Item::ItemDefinition.load
-      
+
       # Equipment
       Lapidary::Equipment.load
     end
-    
+
     def load_config
       WORLD.shop_manager.load_shops
       WORLD.door_manager.load_single_doors
       WORLD.door_manager.load_double_doors
-      
+
       Lapidary::World::NPCSpawns.load
       Lapidary::World::ItemSpawns.load
     end
-    
+
     # Binds the server socket and begins accepting player connections.
     def bind
       EventMachine.run do
         Signal.trap('INT') do
           WORLD.players.each {|p| WORLD.unregister(p) }
-          
+
           sleep(0.01) while WORLD.work_thread.waiting.positive?
-          
+
           EventMachine.stop if EventMachine.reactor_running?
           exit
         end
-        
+
         Signal.trap('TERM') { EventMachine.stop }
-        
+
         EventMachine.start_server('0.0.0.0', @config.port + @version + 1, Lapidary::Net::JaggrabConnection) if $cache
         @log.info "Accepting Jaggrab on port #{@config.port + @version + 1}"
 
